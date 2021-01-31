19e15732cd376e78be38718d61e16403713c6585
Ekaterina Petryaeva <petryaeva.e@gmail.com>
2021-01-31 17:40:40 +0300

    add task sort-strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,21 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+export function sortStrings(arr, param = 'asc') {
+    const compare = (a, b) => {
+        if (param === 'desc') {
+            return b.localeCompare(a, ['ru', 'en'], {caseFirst: 'upper'});
+        } else {
+            return a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'});
+        }
+    }
+
+    return arr.slice().sort((a, b) => compare(a, b));
 }
+
+console.log(sortStrings(['абрикос', 'Абрикос', 'яблоко', 'Яблоко', 'ёжик', 'Ёжик']));
+
+console.log(sortStrings(['apple', 'Apple', 'banana', 'Banana', 'orange', 'Orange']));
+
+console.log(sortStrings(['5', '3', '4', '1', '2', '6']));
